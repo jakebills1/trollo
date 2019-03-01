af45468af13650d3ea9c93d6df9f715cc966282d
Jake Bills <jakebills1@gmail.com>
2019-03-01 09:31:32 -0700

    building out list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,26 +1,41 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
   before_action :set_board
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
   def index
-
+    @lists = @board.lists
   end
 
   def show
   end
 
   def new
+    @list = @board.lists.new
+    render partial: 'form'
+  end
+
+  def create
+
   end
 
   def edit
+    render partial: 'form'
+  end
+
+  def destroy
+    @list.remove_record(@list.id)
   end
 
   private
-  def set_board
-    @board = Board.find(params[:board_id])
-  end
+    def set_board
+      @board = Board.find(params[:board_id])
+    end
 
-  def set_list
-    @list = List.single_list()
-  end
+    def set_list
+      @list = List.single_list(@board.id, params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,13 @@
 class List < ApplicationRecord
   belongs_to :board
+
+  def self.single_list(board, id)
+      List.find_by_sql(["
+      SELECT *
+      FROM lists
+      WHERE lists.id = ? AND lists.board_id = ?
+      ", id, board
+    ]).first
+  end
+
 end
