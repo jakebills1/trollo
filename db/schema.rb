62efe7addd3cb24548b577248809952f27e73a88
Jake Bills <jakebills1@gmail.com>
2019-03-03 11:16:04 -0700

    add todo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_180748) do
+ActiveRecord::Schema.define(version: 2019_03_03_181154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,22 @@ ActiveRecord::Schema.define(version: 2019_03_02_180748) do
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.text "desc"
+    t.boolean "complete", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "desc"
+    t.boolean "complete", default: false
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_todos_on_list_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +62,5 @@ ActiveRecord::Schema.define(version: 2019_03_02_180748) do
 
   add_foreign_key "boards", "users"
   add_foreign_key "lists", "boards"
+  add_foreign_key "todos", "lists"
 end
