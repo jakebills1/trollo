48c4e265579b3f3c6dc1777f08f2b7e6883ec872
Jake Bills <jakebills1@gmail.com>
2019-03-03 09:34:14 -0700

    built out lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,13 +14,27 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = @board.lists.new(list_params)
 
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   def edit
     render partial: 'form'
   end
 
+  def update
+    if @list.update
+      redirect_to [@board, @list]
+    else
+      render :edit
+    end
+  end
+  
   def destroy
     @list.remove_record(@list.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_233725) do
+ActiveRecord::Schema.define(version: 2019_03_02_180748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_02_28_233725) do
     t.bigint "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "priority"
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
